44a81a83491209a3eb2e49fd74bdb11cd0a20970
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-09-05 11:40:58 +0800

    simplified nbody dataset class

--- dataset/nbody.py
+++ dataset/nbody.py
@@ -506,25 +506,54 @@ def simulate():
                     save_path='6body.gif')
 
 
-def gen(): 
-    """
-    Lever: 
-    1. n_context: control # of in-context samples
-    2. stride: control Delta t between samples
-    3. T: control total time of simulation
-    4. patterns: control the type of simulation (cartesian, orbital), orbital is hiearchical system with one dominating body
-    6. n_bodies: control the number of bodies in the simulation
-    """
 
-    dataset = create_dataset_with_params(
-        n_bodies=3,
-        patterns=['orbital'],
-        n_context=3,
-        stride=1,
-        T=1000,
-        include_masses=True,
-        answer_token='<a>'
-    )
+# Datset Class 
+# --------------------------------------------------------------------------------------------------------------------------
+from base import BaseDataset, compute_hier_seq_len
+from dataclasses import dataclass, field
+from typing import List
+import numpy as np
+import re
+from matplotlib import pyplot as plt
+from typing import List, Optional, Tuple
 
-    tok = TinyTokenizer()
-    s_enc = tok(dataset['sequences'][0])
+
+@dataclass 
+class NBodyDataset(BaseDataset): 
+    n_bodies: int = 2
+    patterns: List[str] = field(default_factory=lambda: ['cartesian'])
+    n_context: int = 3
+    stride: int = 1
+    T: int = 10
+    include_masses: bool = True
+    K: int = 2
+    L: int = 3
+    filepath: str = 'dataset/nbody/sequences.bin'
+    tokenizer: TinyTokenizer = TinyTokenizer()
+    answer_token: str = TinyTokenizer().answer_token
+    answer_token_id: int = TinyTokenizer().answer_token_id
+
+    def build(self): 
+        # Generate raw data
+        dataset = create_dataset_with_params(
+            n_bodies=self.n_bodies,
+            patterns=self.patterns,
+            n_context=self.n_context,
+            stride=self.stride,
+            T=self.T,
+            include_masses=self.include_masses,
+            answer_token=self.answer_token,
+        )
+        
+        # Tokenize sequences
+        self.vocab_size = self.tokenizer.vocab_size
+        self.sequences = [self.tokenizer(s) for s in dataset['sequences']]
+        self.lengths = [compute_hier_seq_len(seq, self.L, self.K) for seq in self.sequences]
+
+        if self.num_data is not None:
+            self.sequences = self.sequences[:self.num_data]
+            self.lengths = self.lengths[:self.num_data]
+        
+        # Save to disk
+        self._save()
+        return self
